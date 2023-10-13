902d65eff1b09af965c08c6ef326e706d3017b3c
Rossario Catherine Elfrida <99484551+catherineelfrida@users.noreply.github.com>
2023-10-14 03:04:14 +0800

    Update create_and_insert.sql

--- challenge3/create_and_insert.sql
+++ challenge3/create_and_insert.sql
@@ -21,22 +21,37 @@ CREATE TABLE transaksi (
   jumlah DECIMAL(10, 2)
 );
 
--- Insert data ke tabel Nasabah
+-- Operasi Create data ke tabel Nasabah
 INSERT INTO nasabah (nama, alamat) VALUES
   ('Diaz', 'Jl. Nasabah 1'),
   ('Theo', 'Jl. Nasabah 2'),
   ('Christine', 'Jl. Nasabah 3');
 
--- Insert data ke tabel Akun
+-- Operasi Create data ke tabel Akun
 INSERT INTO akun (nasabah_id, jenis, saldo) VALUES
   (1, 'Tabungan', 1000.00),
   (1, 'Giro', 500.00),
   (2, 'Tabungan', 1500.00),
   (3, 'Tabungan', 2000.00);
 
--- Insert data ke tabel Transaksi
+-- Operasi Create data ke tabel Transaksi
 INSERT INTO transaksi (akun_id, jenis, jumlah) VALUES
   (1, 'Setor', 500.00),
   (1, 'Tarik', 200.00),
   (2, 'Setor', 300.00),
   (3, 'Setor', 1000.00);
+
+-- Operasi Read di tabel nasabah
+SELECT * FROM nasabah;
+
+-- Operasi Read di tabel akun
+SELECT * FROM akun;
+
+-- Operasi Read di tabel transaksi
+SELECT * FROM transaksi;
+
+-- Operasi Update
+UPDATE akun SET saldo = 1500.00 WHERE id = 1;
+
+-- Operasi Delete
+DELETE FROM transaksi WHERE id = 1;
