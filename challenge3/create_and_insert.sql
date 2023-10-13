b81c2d1a364366815eb52d0af917a06ced807b15
Rossario Catherine Elfrida <roselfridaa@gmail.com>
2023-10-14 02:29:56 +0800

    + folder challenge3

---
+++ challenge3/create_and_insert.sql
@@ -0,0 +1,42 @@
+-- Tabel Nasabah
+CREATE TABLE nasabah (
+  id BIGSERIAL PRIMARY KEY,
+  nama VARCHAR(255),
+  alamat VARCHAR(255)
+);
+
+-- Tabel Akun
+CREATE TABLE akun (
+  id BIGSERIAL PRIMARY KEY,
+  nasabah_id BIGSERIAL REFERENCES nasabah(id),
+  jenis VARCHAR(50),
+  saldo DECIMAL(10, 2)
+);
+
+-- Tabel Transaksi
+CREATE TABLE transaksi (
+  id BIGSERIAL PRIMARY KEY,
+  akun_id BIGSERIAL REFERENCES akun(id),
+  jenis VARCHAR(50),
+  jumlah DECIMAL(10, 2)
+);
+
+-- Insert data ke tabel Nasabah
+INSERT INTO nasabah (nama, alamat) VALUES
+  ('Diaz', 'Jl. Nasabah 1'),
+  ('Theo', 'Jl. Nasabah 2'),
+  ('Christine', 'Jl. Nasabah 3');
+
+-- Insert data ke tabel Akun
+INSERT INTO akun (nasabah_id, jenis, saldo) VALUES
+  (1, 'Tabungan', 1000.00),
+  (1, 'Giro', 500.00),
+  (2, 'Tabungan', 1500.00),
+  (3, 'Tabungan', 2000.00);
+
+-- Insert data ke tabel Transaksi
+INSERT INTO transaksi (akun_id, jenis, jumlah) VALUES
+  (1, 'Setor', 500.00),
+  (1, 'Tarik', 200.00),
+  (2, 'Setor', 300.00),
+  (3, 'Setor', 1000.00);
